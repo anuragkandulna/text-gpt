a927ff98ab1eb92c3d981f13c9e9377bec1f2fef
Anurag Kandulna <dev.ak@outlook.in>
2024-08-15 15:49:40 +0530

    added Card

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 function Card() {
   return (
-    <div className="card bg-primary text-primary-content w-96">
+    <div className="card-normal bg-primary text-primary-content w-auto">
         <div className="card-body">
             <h2 className="card-title">Online Video Summarizer</h2>
             <p>Transcribe Summarize & Translate in 1 simple click!</p>
             <div className="card-actions justify-end">
-            <button className="btn">Start</button>
+                <div className="form-control">
+                    <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+                </div>
+                <button className="btn">Start</button>
             </div>
         </div>
         </div>
