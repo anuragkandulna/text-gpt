f3e134c091eb3423271bca59dbf3f3f8a5dfafa7
Anurag Kandulna <dev.ak@outlook.in>
2024-08-16 01:25:33 +0530

    adjusted App.jsx for visibility

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Navbar from './components/Navbar'
-import Card from './components/Card'
-import Footer from './components/Footer'
+import './App.css';
+import Navbar from './components/Navbar';
+import Card from './components/Card';
+import Footer from './components/Footer';
+import AudioPlayer from './components/AudioPlayer';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <Navbar />
-      <Card
-        title="Card Title"
-        description="This is a description of the card content. It gives a brief overview of the topic."
-        link="#"
-        inputPlaceholder="Type something here..."
-      />
-      <Footer />
+    <div className='w-full h-screen justify-between'>
+      {/* <div id='navBarContainer'> */}
+        <Navbar />
+      {/* </div> */}
+      <div id='cardContainer'>
+        <Card
+            title="Card Title"
+            description="This is a description of the card content. It gives a brief overview of the topic."
+            inputPlaceholder="Type something here..."
+          />
+      </div>
+      <div id='audioPlayerContainer'
+        className='p-4 flex-grow'>
+        <AudioPlayer videoId="dQw4w9WgXcQ" />
+      </div>
+      <div id='footerContainer'
+        className='fixed bottom-0 w-full'>
+        <Footer />
+      </div> 
+    </div>
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
