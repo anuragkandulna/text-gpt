b4ced81108c5da81fa345193fd80c6363aed4a7d
Anurag Kandulna <dev.ak@outlook.in>
2024-08-16 03:21:44 +0530

    Refactored App.jsx to divine

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,31 +3,14 @@ import Navbar from './components/Navbar';
 import Card from './components/Card';
 import Footer from './components/Footer';
 import AudioPlayer from './components/AudioPlayer';
+import YTSearch from './components/YTSearch';
 
 function App() {
   return (
     <>
-    <div className='w-full h-screen justify-between'>
-      {/* <div id='navBarContainer'> */}
-        <Navbar />
-      {/* </div> */}
-      <div id='cardContainer'>
-        <Card
-            title="Card Title"
-            description="This is a description of the card content. It gives a brief overview of the topic."
-            inputPlaceholder="Type something here..."
-          />
-      </div>
-      <div id='audioPlayerContainer'
-        className='p-4 flex-grow'>
-        <AudioPlayer videoId="dQw4w9WgXcQ" />
-      </div>
-      <div id='footerContainer'
-        className='fixed bottom-0 w-full'>
-        <Footer />
-      </div> 
-    </div>
-
+      <Navbar />
+      <YTSearch />
+      <Footer />
     </>
   );
 }
