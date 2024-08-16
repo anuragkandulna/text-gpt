e4f1aab4d48e50e8289b97a571dfec4e19e38dc2
Anurag Kandulna <dev.ak@outlook.in>
2024-08-16 11:52:24 +0530

    Updated App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,15 +4,14 @@ import Card from './components/Card';
 import Footer from './components/Footer';
 import AudioPlayer from './components/AudioPlayer';
 import YTSearch from './components/YTSearch';
+import ProductFeatures from './components/ProductFeatures';
 
 function App() {
   return (
     <>
       <Navbar />
-      <YTSearch />
-      <Card />
-      <Card />
-      <Card />
+      {/* <YTSearch /> */}
+      <ProductFeatures />
       <Footer />
     </>
   );
