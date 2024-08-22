9c1be6b698333dad7ec872a1cf8dc9d7d760db2f
Anurag Kandulna <dev.ak@outlook.in>
2024-08-22 17:00:32 +0530

    fixed indetation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,22 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import Card from './components/Card';
-import Footer from './components/Footer';
-import AudioPlayer from './components/AudioPlayer';
-import YTSearch from './components/YTSearch';
-import ProductFeatures from './components/ProductFeatures';
-import UserRegistration from './components/UserRegistration';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Card from "./components/Card";
+import Footer from "./components/Footer";
+import AudioPlayer from "./components/AudioPlayer";
+import YTSearch from "./components/YTSearch";
+import ProductFeatures from "./components/ProductFeatures";
+import UserRegistration from "./components/UserRegistration";
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      {/* <YTSearch /> */}
-      <ProductFeatures />
-      {/* <UserRegistration /> */}
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <Navbar />
+            {/* <YTSearch /> */}
+            <ProductFeatures />
+            {/* <UserRegistration /> */}
+            <Footer />
+        </>
+    );
 }
 
 export default App;
--- frontend/src/components/AudioPlayer.jsx
+++ frontend/src/components/AudioPlayer.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const AudioPlayer = ({ videoId }) => {
-  return (
-    <div className="w-full flex justify-center items-center">
-      <iframe
-        width="300"
-        height="150"
-        src={`https://www.youtube.com/embed/${videoId}?autoplay=0`}
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-        title="YouTube Audio Player"
-      ></iframe>
-    </div>
-  );
+    return (
+        <div className="w-full flex justify-center items-center">
+            <iframe
+                width="300"
+                height="150"
+                src={`https://www.youtube.com/embed/${videoId}?autoplay=0`}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="YouTube Audio Player"
+            ></iframe>
+        </div>
+    );
 };
 
 export default AudioPlayer;
