3ea62a86140658771d1a74124781442e1f6b6076
Anurag Kandulna <dev.ak@outlook.in>
2024-08-25 16:39:05 +0530

    Updated Card to modal floating window

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,122 +1,65 @@
-import {
-    CloudArrowUpIcon,
-    LockClosedIcon,
-    ServerIcon,
-} from "@heroicons/react/20/solid";
+"use client";
 
-export default function Card() {
+import { useState } from "react";
+import {
+    Dialog,
+    DialogBackdrop,
+    DialogPanel,
+    DialogTitle,
+} from "@headlessui/react";
+import { CheckIcon } from "@heroicons/react/24/outline";
+
+export default function Example() {
+    const [open, setOpen] = useState(true);
+
     return (
-        <div className="relative bg-white">
-            <div className="mx-auto max-w-7xl lg:flex lg:justify-between lg:px-8 xl:justify-end">
-                <div className="lg:flex lg:w-1/2 lg:shrink lg:grow-0 xl:absolute xl:inset-y-0 xl:right-1/2 xl:w-1/2">
-                    <div className="relative h-80 lg:-ml-8 lg:h-auto lg:w-full lg:grow xl:ml-0">
-                        <img
-                            alt=""
-                            src="https://images.unsplash.com/photo-1559136555-9303baea8ebd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-x=.4&w=2560&h=3413&&q=80"
-                            className="absolute inset-0 h-full w-full bg-gray-50 object-cover"
-                        />
-                    </div>
-                </div>
-                <div className="px-6 lg:contents">
-                    <div className="mx-auto max-w-2xl pb-24 pt-16 sm:pb-32 sm:pt-20 lg:ml-8 lg:mr-0 lg:w-full lg:max-w-lg lg:flex-none lg:pt-32 xl:w-1/2">
-                        <p className="text-base font-semibold leading-7 text-indigo-600">
-                            Deploy faster
-                        </p>
-                        <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                            A better workflow
-                        </h1>
-                        <p className="mt-6 text-xl leading-8 text-gray-700">
-                            Aliquet nec orci mattis amet quisque ullamcorper
-                            neque, nibh sem. At arcu, sit dui mi, nibh dui, diam
-                            eget aliquam. Quisque id at vitae feugiat egestas
-                            ac. Diam nulla orci at in viverra scelerisque eget.
-                            Eleifend egestas fringilla sapien.
-                        </p>
-                        <div className="mt-10 max-w-xl text-base leading-7 text-gray-700 lg:max-w-none">
-                            <p>
-                                Faucibus commodo massa rhoncus, volutpat.
-                                Dignissim sed eget risus enim. Mattis mauris
-                                semper sed amet vitae sed turpis id. Id dolor
-                                praesent donec est. Odio penatibus risus viverra
-                                tellus varius sit neque erat velit. Faucibus
-                                commodo massa rhoncus, volutpat. Dignissim sed
-                                eget risus enim. Mattis mauris semper sed amet
-                                vitae sed turpis id.
-                            </p>
-                            <ul
-                                role="list"
-                                className="mt-8 space-y-8 text-gray-600"
-                            >
-                                <li className="flex gap-x-3">
-                                    <CloudArrowUpIcon
-                                        aria-hidden="true"
-                                        className="mt-1 h-5 w-5 flex-none text-indigo-600"
-                                    />
-                                    <span>
-                                        <strong className="font-semibold text-gray-900">
-                                            Push to deploy.
-                                        </strong>{" "}
-                                        Lorem ipsum, dolor sit amet consectetur
-                                        adipisicing elit. Maiores impedit
-                                        perferendis suscipit eaque, iste dolor
-                                        cupiditate blanditiis ratione.
-                                    </span>
-                                </li>
-                                <li className="flex gap-x-3">
-                                    <LockClosedIcon
-                                        aria-hidden="true"
-                                        className="mt-1 h-5 w-5 flex-none text-indigo-600"
-                                    />
-                                    <span>
-                                        <strong className="font-semibold text-gray-900">
-                                            SSL certificates.
-                                        </strong>{" "}
-                                        Anim aute id magna aliqua ad ad non
-                                        deserunt sunt. Qui irure qui lorem
-                                        cupidatat commodo.
-                                    </span>
-                                </li>
-                                <li className="flex gap-x-3">
-                                    <ServerIcon
-                                        aria-hidden="true"
-                                        className="mt-1 h-5 w-5 flex-none text-indigo-600"
-                                    />
-                                    <span>
-                                        <strong className="font-semibold text-gray-900">
-                                            Database backups.
-                                        </strong>{" "}
-                                        Ac tincidunt sapien vehicula erat auctor
-                                        pellentesque rhoncus. Et magna sit morbi
-                                        lobortis.
-                                    </span>
-                                </li>
-                            </ul>
-                            <p className="mt-8">
-                                Et vitae blandit facilisi magna lacus commodo.
-                                Vitae sapien duis odio id et. Id blandit
-                                molestie auctor fermentum dignissim. Lacus diam
-                                tincidunt ac cursus in vel. Mauris varius
-                                vulputate et ultrices hac adipiscing egestas.
-                                Iaculis convallis ac tempor et ut. Ac lorem vel
-                                integer orci.
-                            </p>
-                            <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">
-                                No server? No problem.
-                            </h2>
-                            <p className="mt-6">
-                                Id orci tellus laoreet id ac. Dolor, aenean leo,
-                                ac etiam consequat in. Convallis arcu ipsum urna
-                                nibh. Pharetra, euismod vitae interdum mauris
-                                enim, consequat vulputate nibh. Maecenas
-                                pellentesque id sed tellus mauris, ultrices
-                                mauris. Tincidunt enim cursus ridiculus mi.
-                                Pellentesque nam sed nullam sed diam turpis
-                                ipsum eu a sed convallis diam.
-                            </p>
+        <Dialog open={open} onClose={setOpen} className="relative z-10">
+            <DialogBackdrop
+                transition
+                className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
+            />
+
+            <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+                <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                    <DialogPanel
+                        transition
+                        className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-sm sm:p-6 data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
+                    >
+                        <div>
+                            <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
+                                <CheckIcon
+                                    aria-hidden="true"
+                                    className="h-6 w-6 text-green-600"
+                                />
+                            </div>
+                            <div className="mt-3 text-center sm:mt-5">
+                                <DialogTitle
+                                    as="h3"
+                                    className="text-base font-semibold leading-6 text-gray-900"
+                                >
+                                    Payment successful
+                                </DialogTitle>
+                                <div className="mt-2">
+                                    <p className="text-sm text-gray-500">
+                                        Lorem ipsum dolor sit amet consectetur
+                                        adipisicing elit. Consequatur amet
+                                        labore.
+                                    </p>
+                                </div>
+                            </div>
                         </div>
-                    </div>
+                        <div className="mt-5 sm:mt-6">
+                            <button
+                                type="button"
+                                onClick={() => setOpen(false)}
+                                className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            >
+                                Go back to dashboard
+                            </button>
+                        </div>
+                    </DialogPanel>
                 </div>
             </div>
-        </div>
+        </Dialog>
     );
 }
