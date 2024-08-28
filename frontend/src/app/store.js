8ea0cedcef467eaf19c083631e78680e56dddc46
Anurag Kandulna <dev.ak@outlook.in>
2024-08-28 23:38:26 +0530

    fixed reducer bindings

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -3,11 +3,11 @@
 */
 
 import { configureStore } from "@reduxjs/toolkit";
-import { userReducer } from "../features/user/userSlice";
+import { loginUser } from "../features/user/userSlice";
 
 export const store = configureStore({
     // TODO: Add configure store
     reducer: {
-        user: userReducer,
+        user: loginUser,
     },
 });
