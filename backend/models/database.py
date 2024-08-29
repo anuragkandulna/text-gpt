e2810d359a217f5636845a38c34700372ee24c3a
Anurag Kandulna <dev.ak@outlook.in>
2024-08-30 00:38:26 +0530

    updated dataabase model

--- backend/models/database.py
+++ backend/models/database.py
@@ -2,14 +2,16 @@
 
 from pymongo import MongoClient
 
-class DatabaseClient:
+
+class DatabaseConnection:
     def __init__(self) -> None:
+        """Connect to database textgpt."""
         self.client = MongoClient('mongodb+srv://baron:agxscoCGbSly2aXw@cluster91181.jkqew.mongodb.net/test?retryWrites=true&w=majority')
         self.db = self.client['textgpt']
 
 
-    def getTable(self, db_table):
-        """Get object to entire database table."""
+    def get_collection(self, db_table):
+        """Get database collection object."""
         try:
             self.collection = self.db[db_table]
             return self.collection
