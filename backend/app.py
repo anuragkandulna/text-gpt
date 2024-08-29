7d429260a31d788d5465de7d1a7ee08be0b200c3
Anurag Kandulna <dev.ak@outlook.in>
2024-08-30 03:31:39 +0530

    cleaned up app.py and optimized code

--- backend/app.py
+++ backend/app.py
@@ -1,27 +1,21 @@
 from flask import Flask, request, jsonify
 from flask_bcrypt import Bcrypt
-from pymongo import MongoClient
 from flask_cors import CORS
 from datetime import datetime, timezone
+from models.custom_logger import CustomLogger 
+from models.user import User as user
 
+
+# Defined defaults
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
-
-# CORS(app)  # This will enable CORS for all routes
-# CORS(app, resources={r"/api/*": {"origins": "http://localhost:5173"}})
+LOGGER = CustomLogger(__name__, level=20).get_logger()
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 
-# Connect to MongoDB Atlas
-# mongodb+srv://baron:agxscoCGbSly2aXw@cluster91181.jkqew.mongodb.net/test?retryWrites=true&w=majority
-client = MongoClient('mongodb+srv://baron:agxscoCGbSly2aXw@cluster91181.jkqew.mongodb.net/test?retryWrites=true&w=majority')
-db = client['textgpt']  # Replace with your database name
-users_collection = db['users']
-
-
-@app.route('/test', methods=['GET'])
+@app.route('/api/test', methods=['GET'])
 def test():
-    print("Test route hit!")
+    LOGGER.info('Test route hit!!!')
     return jsonify({"message": "Test route is working!"}), 200
 
 
@@ -33,20 +27,26 @@ def login():
         username = data.get('username')
         password = data.get('password')
 
-        user = users_collection.find_one({"username": username})
-        if not user:
-            return jsonify({"message": "User not found"}), 404
+        user_data = user.find_by_username({"username": username})
+        if not user_data:
+            LOGGER.info(f'{username} not found in database')
+            return jsonify({"message": f"{username} User not found!"}), 404
 
-        stored_hash = user.get('password_hash')
+        stored_hash = user_data.get('password_hash')
 
         if bcrypt.check_password_hash(stored_hash, password):
-            print(f'{username} successfully logged in on {datetime.now(timezone.utc).strftime('%Y-%m-%d %H:%M:%S %Z')}')
-            return jsonify({"message": "Login successful!", "user_id": str(user['_id'])}), 200
+            current_ts = datetime.now(timezone.utc).strftime('%Y-%m-%d %H:%M:%S %Z')
+            LOGGER.info(f'{current_ts}: {username} successfully logged in')
+            return jsonify({"message": f"{username} Login successful!"}), 200
+
         else:
-            return jsonify({"message": "Incorrect password"}), 403
+            LOGGER.info(f'{current_ts}: {username} Incorrect password!!!')
+            return jsonify({"message": "Incorrect password!!!"}), 403
+
     except Exception as e:
-        print(f'Exception in user login API/login: {e}')
+        LOGGER.info(f'Exception in user login API/v1/login: {e}')
         return jsonify({"message": "An error occurred"}), 500
+
 
 # Register route (if you need to add new users)
 @app.route('/api/v1/register', methods=['POST'])
@@ -55,37 +55,21 @@ def register():
         data = request.json
 
         username = data.get('username')
-        email = data.get('username')
         password = data.get('password')
-        # first_name = data.get('first_name')
-        # last_name = data.get('last_name')
-        created_at = data.get('created_at')
-        updated_at = data.get('updated_at')
 
         # Hash the password
         password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
 
-        # Prepare the user document to insert into MongoDB
-        user = {
-            "username": username,
-            "email": email,
-            "password_hash": password_hash,
-            # "first_name": first_name,
-            # "last_name": last_name,
-            "role": "user",
-            "is_active": True,
-            "created_at": created_at,
-            "updated_at": updated_at
-        }
+        user.create_new_user(
+            username=username,
+            password_hash=password_hash
+        )
 
-        # Insert the user into the database
-        result = users_collection.insert_one(user)
-
-        # Return success response
-        return jsonify({"message": "User registered successfully!"}), 201
+        LOGGER.info(f"{username} User registered successfully!")
+        return jsonify({"message": f"{username} User registered successfully!"}), 201
     
     except Exception as e:
-        print(f'Exception in new user registration API/register: {e}')
+        LOGGER.info(f'Exception in new user registration API/v1/register: {e}')
         return jsonify({"message": "An error occurred during registration"}), 500
 
 if __name__ == '__main__':
