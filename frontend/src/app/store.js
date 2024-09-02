8ede02e0543eeb1c3ea192598d282ec4abc0ee24
Anurag Kandulna <dev.ak@outlook.in>
2024-09-03 00:18:05 +0530

    Fixed code issues with store.js

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -3,11 +3,11 @@
 */
 
 import { configureStore } from "@reduxjs/toolkit";
-import { loginUser } from "../features/user/userSlice";
+import userReducer from "../features/user/userSlice";
 
 export const store = configureStore({
     // TODO: Add configure store
     reducer: {
-        user: loginUser,
+        user: userReducer,
     },
 });
