3b7a6dd23cb0ee7f24024ce3b09d31ce6b9f7491
Anurag Kandulna <dev.ak@outlook.in>
2024-09-04 01:38:35 +0530

    Updated Layout.jsx with stick header and footer

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -6,9 +6,18 @@ import { Outlet } from "react-router-dom";
 function Layout() {
     return (
         <>
-            <Header />
-            <Outlet />
-            <Footer />
+            <div className="flex flex-col min-h-screen">
+                {/* Sticky Header */}
+                <Header className="sticky top-0 z-50 bg-white shadow" />
+
+                {/* Main Content */}
+                <main className="flex-grow">
+                    <Outlet />
+                </main>
+
+                {/* Sticky Footer */}
+                <Footer className="sticky bottom-0 z-50 bg-gray-800 text-white py-4" />
+            </div>
         </>
     );
 }
