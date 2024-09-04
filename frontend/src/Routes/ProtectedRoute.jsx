075210dbb1ae19566b95851a561d5199657dc8bc
Anurag Kandulna <dev.ak@outlook.in>
2024-09-04 16:55:40 +0530

    updated ProtectedRoute.jsx to render Dashboard only when user is authenticated

--- frontend/src/Routes/ProtectedRoute.jsx
+++ frontend/src/Routes/ProtectedRoute.jsx
@@ -1,3 +1,17 @@
-export default function ProtectedRoute() {
-    return <></>;
+import React from "react";
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import Dashboard from "../components/Dashboard/Dashboard";
+import ProductFeatures from "../components/ProductFeatures/ProductFeatures";
+
+function ProtectedRoute() {
+    const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
+
+    if (isAuthenticated) {
+        return <Dashboard />;
+    }
+
+    return <ProductFeatures />;
 }
+
+export default ProtectedRoute;
