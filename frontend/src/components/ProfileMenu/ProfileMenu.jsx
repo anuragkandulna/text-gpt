691235cb0882c0eccf5f15e442486eb27eff48c2
Anurag Kandulna <dev.ak@outlook.in>
2024-09-04 21:12:12 +0530

    updated ProfileMenu.jsx with correct tags

--- frontend/src/components/ProfileMenu/ProfileMenu.jsx
+++ frontend/src/components/ProfileMenu/ProfileMenu.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/react";
 import { UserCircleIcon } from "@heroicons/react/24/outline";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function ProfileMenu() {
     const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
@@ -42,28 +42,28 @@ export default function ProfileMenu() {
                     className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
                 >
                     <MenuItem>
-                        <a
+                        <Link
                             href="#"
                             className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
                         >
                             Your Profile
-                        </a>
+                        </Link>
                     </MenuItem>
                     <MenuItem>
-                        <a
+                        <Link
                             href="#"
                             className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
                         >
                             Settings
-                        </a>
+                        </Link>
                     </MenuItem>
                     <MenuItem>
-                        <a
+                        <Link
                             href="#"
                             className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
                         >
                             Sign out
-                        </a>
+                        </Link>
                     </MenuItem>
                 </MenuItems>
             )}
