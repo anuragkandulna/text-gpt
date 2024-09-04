be20960c171955860cb9fb2b75a67b26f7c51cf2
Anurag Kandulna <dev.ak@outlook.in>
2024-09-05 02:06:51 +0530

    simplified Dashboard.jsx

--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -43,45 +43,38 @@ export default function Example() {
         <body class="h-full">
         ```
       */}
-            <div className="min-h-full">
-                <header className="bg-white shadow">
-                    <div className="flex">
-                        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 left-0">
-                            <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900">
-                                Dashboard
-                            </h1>
-                        </div>
-                        <div className="mx-auto right-0 py-1 max-w-7xl">
-                            <form className="mt-5 sm:flex sm:items-center">
-                                <div className="w-full sm:max-w-xs flex">
-                                    <label htmlFor="email" className="sr-only">
-                                        Email
-                                    </label>
-                                    <input
-                                        id="email"
-                                        name="email"
-                                        type="email"
-                                        placeholder="you@example.com"
-                                        className="block w-full rounded-l-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                    />
-                                    <button
-                                        type="submit"
-                                        className="inline-flex items-center justify-center rounded-r-md bg-indigo-600 px-3 py-2 text-md font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                                    >
-                                        Start
-                                    </button>
-                                </div>
-                            </form>
-                        </div>
+            <div className="overflow-hidden rounded-lg bg-white shadow items-center">
+                <div className="px-4 py-5 sm:p-6">
+                    {/* Content goes here */}
+
+                    <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900 text-center">
+                        Dashboard
+                    </h1>
+                    <div className="text-md text-center">
+                        Transcribe, Translate & Summarize any video!
                     </div>
-                </header>
-            </div>
-            <main>
-                <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                    {/* Your content */}
+                    <form className="mt-5 sm:flex sm:items-center justify-center">
+                        <div className="w-full sm:max-w-xs flex">
+                            <label htmlFor="email" className="sr-only">
+                                Email
+                            </label>
+                            <input
+                                id="email"
+                                name="email"
+                                type="email"
+                                placeholder="you@example.com"
+                                className="block w-full rounded-l-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                            <button
+                                type="submit"
+                                className="inline-flex items-center justify-center rounded-r-md bg-indigo-600 px-3 py-2 text-md font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            >
+                                Start
+                            </button>
+                        </div>
+                    </form>
                 </div>
-            </main>
-            {/* </div> */}
+            </div>
         </>
     );
 }
