246dc803beed55a095c62fb433e6bc22fd840213
Anurag Kandulna <dev.ak@outlook.in>
2024-09-04 21:26:50 +0530

    created new file companyConstants.js

---
+++ frontend/src/constants/companyConstants.js
@@ -0,0 +1,4 @@
+export const COMPANY_NAME = "TextGPT";
+export const COMPANY_URL =
+    "https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500";
+export const AUTHOR_NAME = "Anurag Kandulna";
