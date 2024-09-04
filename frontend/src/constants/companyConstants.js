346c1a46650b3543d28a87616b4bc5e28aa17348
Anurag Kandulna <dev.ak@outlook.in>
2024-09-04 22:09:13 +0530

    updated company constants

--- frontend/src/constants/companyConstants.js
+++ frontend/src/constants/companyConstants.js
@@ -1,4 +1,4 @@
 export const COMPANY_NAME = "TextGPT";
-export const COMPANY_URL =
+export const COMPANY_LOGO =
     "https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500";
 export const AUTHOR_NAME = "Anurag Kandulna";
