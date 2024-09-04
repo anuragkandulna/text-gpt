a2a3a33902b20de26b9f70f19b4efbce49b05b18
Anurag Kandulna <dev.ak@outlook.in>
2024-09-04 19:46:48 +0530

    added profile menu

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -84,7 +84,7 @@ export default function Navbar() {
                     <div className="flex items-center">
                         <div className="md:ml-4 md:flex md:flex-shrink-0 md:items-center">
                             {/* <Navigate to="/login"> */}
-                            <button
+                            {/* <button
                                 type="button"
                                 className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                             >
@@ -96,8 +96,54 @@ export default function Navbar() {
                                     aria-hidden="true"
                                     className="h-9 w-9"
                                 />
-                            </button>
+                            </button> */}
                             {/* </Navigate> */}
+
+                            {/* Profile dropdown */}
+                            <Menu as="div" className="relative ml-3">
+                                <div>
+                                    <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                                        <span className="absolute -inset-1.5" />
+                                        <span className="sr-only">
+                                            Open user menu
+                                        </span>
+                                        <img
+                                            alt=""
+                                            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                            className="h-8 w-8 rounded-full"
+                                        />
+                                    </MenuButton>
+                                </div>
+                                <MenuItems
+                                    transition
+                                    className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+                                >
+                                    <MenuItem>
+                                        <a
+                                            href="#"
+                                            className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
+                                        >
+                                            Your Profile
+                                        </a>
+                                    </MenuItem>
+                                    <MenuItem>
+                                        <a
+                                            href="#"
+                                            className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
+                                        >
+                                            Settings
+                                        </a>
+                                    </MenuItem>
+                                    <MenuItem>
+                                        <a
+                                            href="#"
+                                            className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
+                                        >
+                                            Sign out
+                                        </a>
+                                    </MenuItem>
+                                </MenuItems>
+                            </Menu>
                         </div>
                     </div>
                 </div>
