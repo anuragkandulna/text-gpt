12323202d512699ea900fd50f9d7c32e2ee587eb
Anurag Kandulna <dev.ak@outlook.in>
2024-09-04 20:06:35 +0530

    new component ProfileMenu.jsx

---
+++ frontend/src/components/ProfileMenu/ProfileMenu.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/react";
+import { UserCircleIcon } from "@heroicons/react/24/outline";
+import { useNavigate } from "react-router-dom";
+
+export default function ProfileMenu() {
+    const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
+    const navigate = useNavigate();
+
+    const handleProfileClick = () => {
+        if (!isAuthenticated) {
+            navigate("/login");
+        }
+    };
+
+    return (
+        <Menu as="div" className="relative ml-3">
+            <div>
+                <MenuButton
+                    onClick={handleProfileClick}
+                    className={`relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 ${
+                        !isAuthenticated ? "cursor-pointer" : ""
+                    }`}
+                >
+                    <span className="absolute -inset-1.5" />
+                    <span className="sr-only">Open user menu</span>
+                    {isAuthenticated ? (
+                        <img
+                            alt="User"
+                            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                            className="h-8 w-8 rounded-full"
+                        />
+                    ) : (
+                        <UserCircleIcon className="h-8 w-8 text-gray-300" />
+                    )}
+                </MenuButton>
+            </div>
+            {isAuthenticated && (
+                <MenuItems
+                    transition
+                    className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+                >
+                    <MenuItem>
+                        <a
+                            href="#"
+                            className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
+                        >
+                            Your Profile
+                        </a>
+                    </MenuItem>
+                    <MenuItem>
+                        <a
+                            href="#"
+                            className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
+                        >
+                            Settings
+                        </a>
+                    </MenuItem>
+                    <MenuItem>
+                        <a
+                            href="#"
+                            className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
+                        >
+                            Sign out
+                        </a>
+                    </MenuItem>
+                </MenuItems>
+            )}
+        </Menu>
+    );
+}
