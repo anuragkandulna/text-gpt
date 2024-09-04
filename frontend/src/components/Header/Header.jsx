1fe75b98945ebad3fd1be0edb833e1026d6bfb65
Anurag Kandulna <dev.ak@outlook.in>
2024-09-04 21:14:01 +0530

    optimized code for Header.jsx

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -20,45 +20,19 @@ import { store } from "../../app/store";
 import { useDispatch, useSelector } from "react-redux";
 import ProfileMenu from "../ProfileMenu/ProfileMenu";
 
-const isAuthenticated = false;
-const user = {
-    name: "Tom Cook",
-    email: "tom@example.com",
-    imageUrl:
-        "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-};
-// const navigation = [
-//     { name: "Dashboard", href: "#", current: true },
-//     { name: "Team", href: "#", current: false },
-//     { name: "Projects", href: "#", current: false },
-//     { name: "Calendar", href: "#", current: false },
-// ];
-const userNavigation = [
-    { name: "Your Profile", href: "#" },
-    { name: "Settings", href: "#" },
-    { name: "Sign out", href: "#" },
-];
-
-function classNames(...classes) {
-    return classes.filter(Boolean).join(" ");
-}
-
 export default function Navbar() {
+    // 1. Get isAuthenticated state from the store
+    const { isAuthenticated } = useSelector((state) => state.user);
     const navigate = useNavigate();
-    // const { isAuthenticated } = useSelector((state) => state.user);
-    const { isAuthenticated, isNewUser } = useSelector((state) => state.user);
     const dispatch = useDispatch();
 
-    // 1. Check if user is authenticated when page loads display correct page.
+    // 2. Display correct page based on isAuthenticated when page loads.
     useEffect(() => {
         if (isAuthenticated) {
-            console.log(
-                `User is already authenticated.... ${isAuthenticated} Navigate to Dashboard`
-            );
+            console.log(`User is already authenticated....`);
             navigate("/dashboard");
-            // navigate("");
         } else {
-            console.log(`user is not authenticated... ${isAuthenticated}`);
+            console.log(`user is not authenticated...`);
             navigate("/");
         }
     }, [isAuthenticated, navigate]);
@@ -84,23 +58,6 @@ export default function Navbar() {
                     </div>
                     <div className="flex items-center">
                         <div className="md:ml-4 md:flex md:flex-shrink-0 md:items-center">
-                            {/* <Navigate to="/login"> */}
-                            {/* <button
-                                type="button"
-                                className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                            >
-                                <span className="absolute -inset-1.5" />
-                                <span className="sr-only">
-                                    View notifications
-                                </span>
-                                <UserCircleIcon
-                                    aria-hidden="true"
-                                    className="h-9 w-9"
-                                />
-                            </button> */}
-                            {/* </Navigate> */}
-
-                            {/* Profile dropdown */}
                             <ProfileMenu />
                         </div>
                     </div>
