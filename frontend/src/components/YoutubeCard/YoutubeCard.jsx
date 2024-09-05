1e9b040bf8e8ecf88a926eca4069bcc232d33064
Anurag Kandulna <dev.ak@outlook.in>
2024-09-05 17:06:11 +0530

    create new component YoutubeCard.jsx

---
+++ frontend/src/components/YoutubeCard/YoutubeCard.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+
+export default function YoutubeCard() {
+    return (
+        <>
+            <div className="overflow-hidden rounded-lg bg-white shadow">
+                <div className="px-4 py-5 sm:p-6">
+                    {/* Content goes here */}
+                    <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900 text-center">
+                        Write me up!
+                    </h1>
+                    <div className="text-md text-center">
+                        Transcribe, Translate & Summarize any video!
+                    </div>
+                    <form className="mt-5 sm:flex sm:items-center justify-center">
+                        <div className="w-full sm:max-w-xs flex">
+                            <label htmlFor="email" className="sr-only">
+                                Email
+                            </label>
+                            <input
+                                id="email"
+                                name="email"
+                                type="email"
+                                placeholder="you@example.com"
+                                className="block w-full rounded-l-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                            <button
+                                type="submit"
+                                className="inline-flex items-center justify-center rounded-r-md bg-indigo-600 px-3 py-2 text-md font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            >
+                                Start
+                            </button>
+                        </div>
+                        <div>
+                            <fieldset>
+                                <legend className="text-sm font-semibold leading-6 text-gray-900">
+                                    Notifications
+                                </legend>
+                                <p className="mt-1 text-sm leading-6 text-gray-600">
+                                    How do you prefer to receive notifications?
+                                </p>
+                                <div className="mt-6 space-y-6 sm:flex sm:items-center sm:space-x-10 sm:space-y-0">
+                                    {notificationMethods.map(
+                                        (notificationMethod) => (
+                                            <div
+                                                key={notificationMethod.id}
+                                                className="flex items-center"
+                                            >
+                                                <input
+                                                    defaultChecked={
+                                                        notificationMethod.id ===
+                                                        "email"
+                                                    }
+                                                    id={notificationMethod.id}
+                                                    name="notification-method"
+                                                    type="radio"
+                                                    className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                                                />
+                                                <label
+                                                    htmlFor={
+                                                        notificationMethod.id
+                                                    }
+                                                    className="ml-3 block text-sm font-medium leading-6 text-gray-900"
+                                                >
+                                                    {notificationMethod.title}
+                                                </label>
+                                            </div>
+                                        )
+                                    )}
+                                </div>
+                            </fieldset>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </>
+    );
+}
