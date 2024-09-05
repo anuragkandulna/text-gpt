c15f5a1c7f1d17c152aff87b82b980277371ad3d
Anurag Kandulna <dev.ak@outlook.in>
2024-09-05 17:39:04 +0530

    optimized code in Dashboard.jsx

--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -8,6 +8,7 @@ import {
     MenuItems,
 } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import YoutubeCard from "../YoutubeCard/YoutubeCard";
 
 const user = {
     name: "Tom Cook",
@@ -43,53 +44,7 @@ export default function Dashboard() {
         <body class="h-full">
         ```
       */}
-            {/* LINK SEARCH */}
-            <div className="overflow-hidden rounded-lg bg-white shadow items-center">
-                <div className="px-4 py-5 sm:p-6">
-                    {/* Content goes here */}
-
-                    <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900 text-center">
-                        Dashboard
-                    </h1>
-                    <div className="text-md text-center">
-                        Transcribe, Translate & Summarize any video!
-                    </div>
-                    <form className="mt-5 sm:flex sm:items-center justify-center">
-                        <div className="w-full sm:max-w-xs flex">
-                            <label htmlFor="email" className="sr-only">
-                                Email
-                            </label>
-                            <input
-                                id="email"
-                                name="email"
-                                type="email"
-                                placeholder="you@example.com"
-                                className="block w-full rounded-l-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                            />
-                            <button
-                                type="submit"
-                                className="inline-flex items-center justify-center rounded-r-md bg-indigo-600 px-3 py-2 text-md font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                            >
-                                Start
-                            </button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-
-            {/* AUDIO PLAYER */}
-            <div className="overflow-hidden rounded-lg bg-white shadow">
-                <div className="px-4 py-5 sm:p-6">
-                    {/* Audio Player */}
-                    <audio controls className="w-full">
-                        <source
-                            src="https://www.youtube.com/watch?v=EUeryhp8HSQ"
-                            type="audio/mpeg"
-                        />
-                        Your browser does not support the audio element.
-                    </audio>
-                </div>
-            </div>
+            <YoutubeCard />
         </>
     );
 }
