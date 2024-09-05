b0fc0a94dba670d4cfb771ad8ee552f0d69a52d7
Anurag Kandulna <dev.ak@outlook.in>
2024-09-05 18:07:07 +0530

    fully rounded corners

--- frontend/src/components/YoutubeCard/YoutubeCard.jsx
+++ frontend/src/components/YoutubeCard/YoutubeCard.jsx
@@ -83,7 +83,7 @@ export default function YoutubeCard() {
                                 id="country"
                                 name="country"
                                 autoComplete="country-name"
-                                className="relative block w-full rounded-none rounded-t-md border-0 bg-transparent py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                className="relative block w-full rounded-md border-0 bg-transparent py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             >
                                 <option>United States</option>
                                 <option>Canada</option>
