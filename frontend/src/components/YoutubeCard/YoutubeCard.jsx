611b48ec30e3ceef4cb5971c36a502bbff259b7e
Anurag Kandulna <dev.ak@outlook.in>
2024-09-05 17:20:12 +0530

    added notification methods

--- frontend/src/components/YoutubeCard/YoutubeCard.jsx
+++ frontend/src/components/YoutubeCard/YoutubeCard.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
+const notificationMethods = [
+    { id: "len30", title: "30 secs" },
+    { id: "len60", title: "60 secs" },
+    { id: "len120", title: "120 secs" },
+];
+
 export default function YoutubeCard() {
     return (
         <>
@@ -30,45 +36,6 @@ export default function YoutubeCard() {
                             >
                                 Start
                             </button>
-                        </div>
-                        <div>
-                            <fieldset>
-                                <legend className="text-sm font-semibold leading-6 text-gray-900">
-                                    Notifications
-                                </legend>
-                                <p className="mt-1 text-sm leading-6 text-gray-600">
-                                    How do you prefer to receive notifications?
-                                </p>
-                                <div className="mt-6 space-y-6 sm:flex sm:items-center sm:space-x-10 sm:space-y-0">
-                                    {notificationMethods.map(
-                                        (notificationMethod) => (
-                                            <div
-                                                key={notificationMethod.id}
-                                                className="flex items-center"
-                                            >
-                                                <input
-                                                    defaultChecked={
-                                                        notificationMethod.id ===
-                                                        "email"
-                                                    }
-                                                    id={notificationMethod.id}
-                                                    name="notification-method"
-                                                    type="radio"
-                                                    className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                                                />
-                                                <label
-                                                    htmlFor={
-                                                        notificationMethod.id
-                                                    }
-                                                    className="ml-3 block text-sm font-medium leading-6 text-gray-900"
-                                                >
-                                                    {notificationMethod.title}
-                                                </label>
-                                            </div>
-                                        )
-                                    )}
-                                </div>
-                            </fieldset>
                         </div>
                     </form>
                 </div>
