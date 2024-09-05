11d5a08783afd79fafd6c8ea3c444f9f67893d82
Anurag Kandulna <dev.ak@outlook.in>
2024-09-05 17:39:28 +0530

    Added radio buttons to select video segment length

--- frontend/src/components/YoutubeCard/YoutubeCard.jsx
+++ frontend/src/components/YoutubeCard/YoutubeCard.jsx
@@ -38,6 +38,38 @@ export default function YoutubeCard() {
                             </button>
                         </div>
                     </form>
+                    <fieldset className="text-center mt-4">
+                        <legend className="text-sm font-semibold leading-6 text-gray-900 -indent-24">
+                            Maximum length of each segment?
+                        </legend>
+                        {/* <p className="mt-1 text-sm leading-6 text-gray-600">
+                            How do you prefer to receive notifications?
+                        </p> */}
+                        <div className="mt-2 space-y-6 sm:flex sm:items-center sm:space-x-10 sm:space-y-0 justify-center">
+                            {notificationMethods.map((notificationMethod) => (
+                                <div
+                                    key={notificationMethod.id}
+                                    className="flex items-center"
+                                >
+                                    <input
+                                        defaultChecked={
+                                            notificationMethod.id === "len60"
+                                        }
+                                        id={notificationMethod.id}
+                                        name="notification-method"
+                                        type="radio"
+                                        className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                                    />
+                                    <label
+                                        htmlFor={notificationMethod.id}
+                                        className="ml-3 block text-sm font-medium leading-6 text-gray-900"
+                                    >
+                                        {notificationMethod.title}
+                                    </label>
+                                </div>
+                            ))}
+                        </div>
+                    </fieldset>
                 </div>
             </div>
         </>
