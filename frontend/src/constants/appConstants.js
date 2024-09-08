cdba367e5129902691028be9a0f1437a83bd21d9
Anurag Kandulna <dev.ak@outlook.in>
2024-09-09 01:30:48 +0530

    new file appConstants.js

---
+++ frontend/src/constants/appConstants.js
@@ -0,0 +1,13 @@
+export const VIDEO_SEGMENT_LENGTHS = [
+    { id: "len30", title: "30 secs" },
+    { id: "len60", title: "60 secs" },
+    { id: "len120", title: "120 secs" },
+];
+
+export const SOURCE_VIDEO_LANGUAGES = [
+    { id: "en-IN", language: "English (India)" },
+    { id: "hi-IN", language: "Hindi (India)" },
+    { id: "bn-IN", language: "Bengali (India)" },
+    { id: "en-US", language: "English (United States)" },
+    { id: "en-UK", language: "English (United Kingdom)" },
+];
