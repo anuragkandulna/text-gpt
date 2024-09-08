3427d310312dafcaf0fc808c47655c1cbb51e8c6
Anurag Kandulna <dev.ak@outlook.in>
2024-09-09 00:06:25 +0530

    updated Youtube.jsx

--- frontend/src/components/YoutubeCard/YoutubeCard.jsx
+++ frontend/src/components/YoutubeCard/YoutubeCard.jsx
@@ -3,6 +3,7 @@ import {
     SOURCE_VIDEO_LANGUAGES,
     VIDEO_SEGMENT_LENGTHS,
 } from "../../constants/appConstants";
+import { ExclamationCircleIcon } from "@heroicons/react/20/solid";
 
 export default function YoutubeCard() {
     return (
@@ -30,6 +31,8 @@ export default function YoutubeCard() {
                                 name="inputLink"
                                 type="url"
                                 placeholder="https://www.youtube.com/watch?v=7hKhxDpVzU4"
+                                aria-invalid="true"
+                                aria-describedby="url-error"
                                 className="block w-full rounded-l-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
                             <button
