62a9494f1c440eb6af67ba802e366bdb54ea5a8a
Anurag Kandulna <dev.ak@outlook.in>
2024-09-09 01:56:33 +0530

    create youtubeSlice.js

---
+++ frontend/src/features/youtube/youtubeSlice.js
@@ -0,0 +1,32 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+    id: "unique_ID",
+    title: "project_title",
+    url: "youtube_url",
+    length: 30,
+    sourceVideoLanguageId: "en-IN",
+};
+
+export const youtubeSlice = createSlice({
+    name: "youtube",
+    initialState,
+    // LIST OF ALL REDUCERS
+    reducers: {
+        // 1. Reducer to add valid youtube link
+        createProject: (state, action) => {
+            (state.url = action.payload.url),
+                (state.length = action.payload.length),
+                (state.sourceVideoLanguageId =
+                    action.payload.sourceVideoLanguageId),
+                (state.id = nanoid()),
+                (state.title = action.payload.title);
+        },
+    },
+});
+
+// 2. Export all reducers individually
+export const { createProject } = youtubeSlice.actions;
+
+// 3. Register the reducer to store so that the store is aware of reducers accessing the data
+export default youtubeSlice.reducer;
