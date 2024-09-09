3571101b04637d49980bf27dd90e19ba528d7b40
Anurag Kandulna <dev.ak@outlook.in>
2024-09-09 11:40:20 +0530

    added youtubeReducer to store.js

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -4,10 +4,12 @@
 
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/user/userSlice";
+import youtubeReducer from "../features/youtube/youtubeSlice";
 
 export const store = configureStore({
     // TODO: Add configure store
     reducer: {
         user: userReducer,
+        youtube: youtubeReducer,
     },
 });
