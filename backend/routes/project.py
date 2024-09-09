cce227cf13456c267024e6ba38a7c2455810f851
Anurag Kandulna <dev.ak@outlook.in>
2024-09-09 13:53:30 +0530

    created new file for project management

---
+++ backend/routes/project.py
@@ -0,0 +1,29 @@
+"""
+Routes for project creation and management.
+"""
+
+from flask import Blueprint, request, jsonify
+from models.custom_logger import CustomLogger
+
+
+# Initialize constants: blueprint, loggers, etc
+project_bp = Blueprint('project_bp', __name__)
+LOGGER = CustomLogger(__name__, level=20).get_logger()
+
+
+@project_bp.route('/api/v1/create_new', methods=['POST'])
+def create_new():
+    try:
+        data = request.json
+
+        title = data.get('title')
+        url = data.get('url')
+        length = data.get('length')
+        src_video_lang_id = data.get('source_video_language_id')
+
+        LOGGER.info(f"{title} project created successfully!")
+        return jsonify({"message": f"{title} project created successfully!"}), 201
+    
+    except Exception as e:
+        LOGGER.error(f'Exception in new project creation API/v1/create_new: {e}')
+        return jsonify({"message": "An error when creating new project"}), 500
