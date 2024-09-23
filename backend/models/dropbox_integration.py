6ba036e64da351f889c982932480a6d439b6bf61
Anurag Kandulna <dev.ak@outlook.in>
2024-09-23 20:10:56 +0530

    dropbox integration model script

--- backend/models/dropbox_integration.py
+++ backend/models/dropbox_integration.py
@@ -2,7 +2,43 @@ import dropbox
 import sys
 from dropbox.exceptions import ApiError, AuthError
 
-TOKEN = 'TOKEN'
+
+TOKEN = ''
+
+
+class DropboxIntegration():
+    def __init__(self) -> None:
+        self.username = ''
+        self.user_uuid = ''
+        self.project_id = ''
+        self.project_title = ''
+        self.project_root = ''
+        self.audio_dir = ''
+        self.transcription_dir = ''
+        self.translation_dir = ''
+        self.summary_dir = ''
+        self.seg_audio_files = []
+        self.seg_transcript_files = []
+        self.seg_translation_files = []
+        self.seg_summary_files = []
+        self.final_transcript_file = ''
+        self.final_translation_file = ''
+        self.final_summary_file = ''
+        self.metadata = ''
+
+    def connect(self):
+        """
+        Connect to dropbox.
+        """
+
+
+
+    def register(self, username, user_uuid, ):
+        """
+        Create a new directory for a user upon new registration.
+        """
+        pass
+
 
 
 if __name__ == '__main__':
