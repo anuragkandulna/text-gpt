0d427fd51dd9bd3b8c65322fa39e0ae2ce480644
Anurag Kandulna <dev.ak@outlook.in>
2025-03-11 21:34:43 +0530

    integrated env variables

--- backend/utils/psql_database.py
+++ backend/utils/psql_database.py
@@ -2,6 +2,7 @@ import psycopg2
 from psycopg2 import pool
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from variables import DB_NAME, DB_USER, DB_PASSWORD, DB_HOST
 from utils.custom_logger import CustomLogger
 
 
@@ -19,10 +20,10 @@ class DatabaseConnection:
             self.pool = psycopg2.pool.SimpleConnectionPool(
                 minconn=1,
                 maxconn=10,
-                dbname="textgpt",
-                user="baron",
-                password="1234",
-                host="localhost",
+                dbname=DB_NAME,
+                user=DB_USER,
+                password=DB_PASSWORD,
+                host=DB_HOST,
                 port=5432
             )
 
