5ce7bd8465e210b9cfeffdb03156f57c4af03089
Anurag Kandulna <dev.ak@outlook.in>
2025-03-14 17:50:00 +0530

    updated constants

--- backend/constants/constants.py
+++ backend/constants/constants.py
@@ -1,12 +1,12 @@
 """All project related constants."""
 
 ### Project related constants
-MAX_SUPPORTED_AUDIO_LENGTH_SECS = 30
-AUDIO_SEGMENT_LENGTH_SECS = 10
-TEMP_AUDIO_DIR = "/tmp/{project_id}/audio/"
-TEMP_TRANSCRIPTION_DIR = "/tmp/{project_id}/transcription/"
-TEMP_TRANSLATION_DIR = "/tmp/{project_id}/translation/"
-TEMP_SUMMARY_DIR = "/tmp/{project_id}/translation/"
+MAX_SUPPORTED_AUDIO_LENGTH_SECS = 2
+AUDIO_SEGMENT_LENGTH_SECS = 5
+TEMP_AUDIO_DIR = "/var/tmp/textgpt/{project_id}/audio/"
+TEMP_TRANSCRIPTION_DIR = "/var/tmp/textgpt/{project_id}/transcription/"
+TEMP_TRANSLATION_DIR = "/var/tmp/textgpt/{project_id}/translation/"
+TEMP_SUMMARY_DIR = "/var/tmp/textgpt/{project_id}/translation/"
 AUDIO_SEGMENT_FILE = "audio_{project_id}_segment_{part}.wav"
 TRANSCRIPT_SEGMENT_FILE = "transcript_{project_id}_segment_{part}.txt"
 TRANSLATION_SEGMENT_FILE = "translation_{project_id}_segment_{part}.txt"
