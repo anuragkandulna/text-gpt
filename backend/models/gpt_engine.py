8ee3a737695e299ddb36fc73070ee04adc8fbec9
Anurag Kandulna <dev.ak@outlook.in>
2025-03-14 16:18:26 +0530

    added summarization code

--- backend/models/gpt_engine.py
+++ backend/models/gpt_engine.py
@@ -1,7 +1,8 @@
 from openai import OpenAI
 import os
 from constants.config import OPENAI_API_KEY
-from constants.prompts import TRANSLATE_PROMPT_1
+from constants.prompts import TRANSLATE_PROMPT_1, SUMMARY_PROPMPT_1
+from constants.constants import MAX_TEMPERATURE, MAX_TOKENS
 from utils.custom_logger import CustomLogger
 
 # Logger
@@ -18,41 +19,75 @@ class GPTEngine:
             LOGGER.error("Empty text provided for translation.")
             return None
         
-        prompt = TRANSLATE_PROMPT_1.format(src_language=src_language, target_language=target_language, text=text)
+        prompt_t = TRANSLATE_PROMPT_1.format(src_language=src_language, target_language=target_language, text=text)
 
         try:
-            response = client.chat.completions.create(
+            response_t = client.chat.completions.create(
                 model="gpt-3.5-turbo",
                 messages=[{
                     "role": "user",
-                    "content": prompt
+                    "content": prompt_t
                 }],
-                temperature=0.7,
-                max_tokens=100
+                temperature=MAX_TEMPERATURE,
+                max_tokens=MAX_TOKENS
             )
 
-            translation = response.choices[0].message.content.strip()
+            LOGGER.debug(f"Translation prompt: {prompt_t}")
+            LOGGER.debug(f"Translation response: {response_t}")
+            translation = response_t.choices[0].message.content.strip()
             LOGGER.info(f"Translation successful: {translation}")
             return translation
 
         except Exception as ex:
-            LOGGER.error(f"OpenAPI error: {ex}")
+            LOGGER.error(f"Error occurred when translation: {ex}")
             return None
 
 
-if __name__ == "__main__":
-    obj = GPTEngine()
+    def summarize_text(self, text):
+        """
+        Summarize text while preserving context.
+        """
+        if not text:
+            LOGGER.error("Empty text provided for summarization.")
+            return None
+        
+        prompt_s = SUMMARY_PROPMPT_1.format(text=text)
 
-    inp_text = "How are you?"
-    src = "English"
-    tgt = "Hindi"
+        try:
+            response_s = client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=[{
+                    "role": "user",
+                    "content": prompt_s
+                }],
+                temperature=MAX_TEMPERATURE,
+                max_tokens=MAX_TOKENS
+            )
 
-    opt_text = obj.translate_text(
-        text=inp_text,
-        src_language=src,
-        target_language=tgt
-    )
+            LOGGER.debug(f"Summary prompt: {prompt_s}")
+            LOGGER.debug(f"Summary response: {response_s}")
+            summary = response_s.choices[0].message.content.strip()
+            LOGGER.info(f"Summarization success: {summary}")
+            return summary
 
-    print(opt_text)
+        except Exception as ex:
+            LOGGER.error(f"Error occurred when summarization: {ex}")
+            return None
+
+
+# if __name__ == "__main__":
+#     obj = GPTEngine()
+
+#     inp_text = "How are you?"
+#     src = "English"
+#     tgt = "Hindi"
+
+#     opt_text = obj.translate_text(
+#         text=inp_text,
+#         src_language=src,
+#         target_language=tgt
+#     )
+
+#     print(opt_text)
         
         
