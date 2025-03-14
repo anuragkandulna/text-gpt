9b9b0bd530a18d786925851889320c3a63c68a8f
Anurag Kandulna <dev.ak@outlook.in>
2025-03-14 10:51:42 +0530

    changed logic for gpt engine

--- backend/models/gpt_engine.py
+++ backend/models/gpt_engine.py
@@ -1,27 +1,58 @@
-import openai
+from openai import OpenAI
 import os
-from variables import OPENAI_API_KEY
+from constants.variables import OPENAI_API_KEY
+from constants.prompts import TRANSLATE_PROMPT_1
 from utils.custom_logger import CustomLogger
 
 # Logger
 LOGGER = CustomLogger(__name__, level=10).get_logger()
+client = OpenAI(api_key = OPENAI_API_KEY)
 
 
-class GPTEngine:
-    def __init__(self, api_key=OPENAI_API_KEY):
-        """
-        Initialize OpenAI for text based operations.
-        """
-        self.api_key = api_key
-        openai.api_key = self.api_key
-
-    
-    def translate_text(self, text, target_language):
+class GPTEngine:    
+    def translate_text(self, text, src_language, target_language):
         """
         Translate source text into target language.
         """
         if not text.strip():
             LOGGER.error("Empty text provided for translation.")
             return None
+        
+        prompt = TRANSLATE_PROMPT_1.format(src_language=src_language, target_language=target_language, text=text)
+
+        try:
+            response = client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=[{
+                    "role": "user",
+                    "content": prompt
+                }],
+                temperature=0.7,
+                max_tokens=100
+            )
+
+            translation = response.choices[0].message.content.strip()
+            LOGGER.info(f"Translation successful: {translation}")
+            return translation
+
+        except Exception as ex:
+            LOGGER.error(f"OpenAPI error: {ex}")
+            return None
+
+
+if __name__ == "__main__":
+    obj = GPTEngine()
+
+    inp_text = "How are you?"
+    src = "English"
+    tgt = "Hindi"
+
+    opt_text = obj.translate_text(
+        text=inp_text,
+        src_language=src,
+        target_language=tgt
+    )
+
+    print(opt_text)
         
         
