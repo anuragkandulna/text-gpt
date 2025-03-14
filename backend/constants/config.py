6ddbb7fc795c0cd325e4824317ae685728bd9d59
Anurag Kandulna <dev.ak@outlook.in>
2025-03-14 12:44:08 +0530

    config file for managing environment variables

---
+++ backend/constants/config.py
@@ -0,0 +1,47 @@
+import os
+from dotenv import load_dotenv
+
+# Load environment variables at once.
+load_dotenv()
+
+# Open API Key
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+
+# Dropbox Keys
+DBX_APP_KEY = os.getenv('DBX_APP_KEY')
+DBX_APP_SECRET = os.getenv('DBX_APP_SECRET')
+DBX_TOKEN_FILE = os.getenv('DBX_TOKEN_FILE')
+
+# Database Keys
+DB_CONNECTION_STR = os.getenv('DB_CONNECTION_STR')
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+
+if not OPENAI_API_KEY:
+    raise ValueError('OpenAI api key not found in .env file.')
+
+if not DBX_APP_KEY:
+    raise ValueError('Dropbox App key not found in .env file.')
+
+if not DBX_APP_SECRET:
+    raise ValueError('Dropbox App secret not found in .env file.')
+
+if not DBX_TOKEN_FILE:
+    raise ValueError('Dropbox Token file not found in .env file.')
+
+if not DB_CONNECTION_STR:
+    raise ValueError('Database Connection string not found in .env file.')
+
+if not DB_NAME:
+    raise ValueError('Database Name not found in .env file.')
+
+if not DB_USER:
+    raise ValueError('Database Username not found in .env file.')
+
+if not DB_PASSWORD:
+    raise ValueError('Database Password not found in .env file.')
+
+if not DB_HOST:
+    raise ValueError('Database Host not found in .env file.')
