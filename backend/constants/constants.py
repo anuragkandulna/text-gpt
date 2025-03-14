0f6909db4ac51f10bc88e032ac59a6f6f12686a9
Anurag Kandulna <dev.ak@outlook.in>
2025-03-14 17:56:53 +0530

    updated directories in constants

--- backend/constants/constants.py
+++ backend/constants/constants.py
@@ -3,10 +3,11 @@
 ### Project related constants
 MAX_SUPPORTED_AUDIO_LENGTH_SECS = 2
 AUDIO_SEGMENT_LENGTH_SECS = 5
-TEMP_AUDIO_DIR = "/var/tmp/textgpt/{project_id}/audio/"
-TEMP_TRANSCRIPTION_DIR = "/var/tmp/textgpt/{project_id}/transcription/"
-TEMP_TRANSLATION_DIR = "/var/tmp/textgpt/{project_id}/translation/"
-TEMP_SUMMARY_DIR = "/var/tmp/textgpt/{project_id}/translation/"
+TEMP_TEXTGPT_DIR = "/var/tmp/textgpt"
+TEMP_AUDIO_DIR = f"{TEMP_TEXTGPT_DIR}/{project_id}/audio/"
+TEMP_TRANSCRIPTION_DIR = f"{TEMP_TEXTGPT_DIR}/{project_id}/transcription/"
+TEMP_TRANSLATION_DIR = f"{TEMP_TEXTGPT_DIR}/{project_id}/translation/"
+TEMP_SUMMARY_DIR = f"{TEMP_TEXTGPT_DIR}/{project_id}/translation/"
 AUDIO_SEGMENT_FILE = "audio_{project_id}_segment_{part}.wav"
 TRANSCRIPT_SEGMENT_FILE = "transcript_{project_id}_segment_{part}.txt"
 TRANSLATION_SEGMENT_FILE = "translation_{project_id}_segment_{part}.txt"
