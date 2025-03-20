0f780a8c4c0c9f5a7487827f785858cb7891ecf4
Anurag Kandulna <dev.ak@outlook.in>
2025-03-20 23:53:06 +0530

    changed logic of invocation

--- backend/models/video_converter.py
+++ backend/models/video_converter.py
@@ -68,62 +68,49 @@ class VideoConverter:
 
             # Save audio to file
             audio_file.seek(0)
-            yt_audio = AudioSegment.from_file(audio_file, format="mp4")
+            # yt_audio = AudioSegment.from_file(audio_file, format="mp4")
             LOGGER.info(f'Successfully converted video {self.url} to audio')
 
             # Split the audio into segments
-            audio_segments = self.split_audio_into_segments(yt_audio)
-            self._save_segments_to_wav(project_id=self.project_id, destination_dir=self.local_audio_dir)
-
-            return {
-                "url": self.url,
-                "src_video_title": self.src_video_title,
-                "src_video_length": self.src_video_len_hours,
-                "temp_audio_dir": self.local_audio_dir,
-                "audio_segments": self.audio_file_names,
-                "project_id": self.project_id
-            }
+            audio_segments = self._split_audio_into_segments()
+            if len(audio_segments) == len(self.audio_file_names):
+                self._save_segments_to_wav()
+            else:
+                raise ValueError("Expected number of audio segments did not match.")
 
         except Exception as ex:
             LOGGER.error(f'Failed to process YouTube URL {self.url}: {ex}')
 
 
-    def _split_audio_into_segments(self, audio):
+    def _split_audio_into_segments(self):
         """
         Split processed audio into segments.
         """
-        max_audio_ms = min(MAX_AUDIO_SEGMENT_COUNT * MAX_AUDIO_SEGMENT_LENGTH_SECS * 1000, len(audio))
-        # max_segments = MAX_SUPPORTED_AUDIO_LENGTH_SECS // AUDIO_SEGMENT_LENGTH_SECS
+        max_audio_ms = min(MAX_AUDIO_SEGMENT_COUNT * MAX_AUDIO_SEGMENT_LENGTH_SECS * 1000, len(self.audio_stream))
         segments = []
         segment_duration_ms = MAX_AUDIO_SEGMENT_LENGTH_SECS * 1000
 
         # Iterage through entire audio range and spilt into segments
-        for i in range(0, len(audio), segment_duration_ms):
+        for i in range(0, len(self.audio), segment_duration_ms):
             try:
-                segments.append(audio[i:i + segment_duration_ms])
+                segments.append(self.audio[i:i + segment_duration_ms])
             except:
-                segments.append(audio[i:])
+                segments.append(self.audio[i:])
         
-        # self.num_segments = min(len(segments), max_segments)
         LOGGER.info(f'{self.src_video_title} is cut into {len(segments)} segments.')
-
         return segments
 
 
-    def _save_segments_to_wav(self, segments, project_id):
+    def _save_segments_to_wav(self, segments):
         """
         Save audio segments locally in /tmp.
         """
-        # self.local_audio_dir = TEMP_AUDIO_DIR.format(project_id=project_id)
         if not os.path.exists(self.local_audio_dir):
             os.makedirs(self.local_audio_dir)
             LOGGER.info(f'Created temp dir for audio: {self.local_audio_dir}')
 
-        for i, segment in enumerate(segments):
-            fname = AUDIO_SEGMENT_FILE.format(project_id=project_id, part=i+1)
-            fpath = os.path.join(self.local_audio_dir, fname)
-            self.audio_file_names.append(fname)
-
-            # Save the audio segment to file
-            segment.export(fpath, format="wav")
-            LOGGER.info(f'Saved {fname} audio segment to {fpath}')
+        for i in range(len(segments)):
+            file_path = self.audio_file_names[i]["audio_file_path"]
+            segments[i].export(file_path, format="wav")
+            self.audio_file_names[i]["is_converted"] = True
+            LOGGER.info(f'Saved audio segment to {file_path}')
